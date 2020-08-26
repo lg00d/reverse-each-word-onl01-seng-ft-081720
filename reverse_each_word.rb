82d380b63030da8da29ba3c0fabbd3f112660a6f
elizabeth goodman <egoodman1180@icloud.com>
2020-08-26 18:26:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,3 +7,13 @@ def reverse_each_word(sentence1)
   end
   return reversed_array.join(" ")
 end
+
+def reverse_each_word(sentence1)
+  sentence1 = sentence1.split(" ")
+  reversed_array = []
+  
+  sentence1.collect do |word|
+    reversed_array << word.reverse 
+  end
+  return reversed_array.join(" ")
+end
